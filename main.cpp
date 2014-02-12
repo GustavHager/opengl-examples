524c9b87b717b6aa49c1bc4986cf269ee787c006
Gustav Häger <hager.gustav@gmail.com>
2014-02-12 21:16:30 +0100

    Now does everything but acually draw something

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void error_callback(int error, const char* description){
   printf("%s\n",description);
 }
 
+//GLFW key callback, called on key events
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
   if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
     glfwSetWindowShouldClose(window,GL_TRUE);
@@ -25,12 +26,15 @@ int main(){
     printf("GLFW init failed, exiting \n");
     exit(-1);
   }
-  //set the error callback
+  //tell GLFW where the error callback is
   glfwSetErrorCallback(error_callback);
-
 
   //create a window and an opengl context
   GLFWwindow* window = glfwCreateWindow(640,480,"w1n",NULL,NULL);
+
+  //tell GLFW where the key callback is, so keypresses in the window are sent there
+  glfwSetKeyCallback(window,key_callback);
+
   //make sure the window was created properly
   if(!window){
     glfwTerminate();
@@ -43,7 +47,17 @@ int main(){
 
   //run as long as the window is not closed
   while(!glfwWindowShouldClose(window)){
-    //ruuuuuun!
+    float ratio;
+    int width,height;
+    glfwGetFramebufferSize(window,&width,&height);
+    ratio = (float)width / (float)height;
+    glViewport(0,0,width,height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //code to draw stuff goes here..
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
 
   //destroy the window
